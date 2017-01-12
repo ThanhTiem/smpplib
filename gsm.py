5ef82099860d27536735769828b2981ebcc85d77
Kirill Kirsanov <kirsanov.k@rfibank.ru>
2017-01-12 11:09:22 +0300

    Чистка

--- gsm.py
+++ gsm.py
@@ -60,7 +60,6 @@ def make_parts(text):
         parts = []
         ipart = 1
         uid = random.randint(0, 255)
-        print(starts,"asd")
         for start in starts:
 
             parts.append(b''.join((b'\x05\x00\x03', bytes([uid]),
